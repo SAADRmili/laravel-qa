3d09df487b800430f1795503aa2c60946492ebb2
SAADRMILI <saadrmili45@gmail.com>
2020-01-12 16:15:47 +0100

    Genrate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -25,6 +25,17 @@ class Answer extends Model
         # code...
         return  \Parsedown::instance()->text($this->body);
     }
+    public static function boot()
+    {
+        # code...
+        parent::boot();
 
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+
+       
+    }
 
 }
